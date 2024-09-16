37b6a1f03158582a5b1c81e00cd2b493a2f1d077
zoyron <sgrarora3@gmail.com>
2024-09-16 18:17:01 +0530

    final update for the template

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 /**
  * Base setup
  */
+
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
@@ -18,8 +19,9 @@ const sizes = {
 };
 
 /**
- * Camera and lights
+ * Camera and lights(optional)
  */
+
 // Perspective Camera
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -30,30 +32,13 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.set(3, 3, 3);
 scene.add(camera);
 
-// Lights
-const light = new THREE.SpotLight(0x006769, 100);
-light.position.set(1, 1.25, 1.25);
-scene.add(light);
-
 /**
  * Adding a base mesh
  */
-const geometry = new THREE.DodecahedronGeometry();
-const material = new THREE.MeshLambertMaterial({
-  color: "#468585",
-  emissive: "#468585",
-});
+const geometry = new THREE.BoxGeometry(2, 2, 2, 8, 8, 8);
+const material = new THREE.MeshNormalMaterial();
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
-
-const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
-const boxMaterial = new THREE.MeshLambertMaterial({
-  color: "#0080fa",
-  emissive: "#0080fa",
-});
-const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-boxMesh.position.y = -1.5;
-scene.add(boxMesh);
 
 /**
  * Renderer and Resizing
@@ -92,7 +77,6 @@ const animate = () => {
   // Rotate mesh
   mesh.rotation.x += 0.0125;
   mesh.rotation.y += 0.0125;
-  boxMesh.rotation.y += 0.00625;
 
   // Update controls
   controls.update();
